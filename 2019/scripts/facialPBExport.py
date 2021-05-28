9708cc6582e619b672d2cfa0681a392ed23a02e0
frozenash <hyh1411@hotmail.com>
2021-05-28 17:04:40 +0900

    20210528
    +NXN Tools Updated

--- 2019/scripts/facialPBExport.py
+++ 2019/scripts/facialPBExport.py
@@ -21,14 +21,18 @@ TIME_UNIT = 'ntsc'
 class FacialPBExport():
     def __init__(self,path):
         self.input_path = path
-        self.output_path = self.out_path()
+        self.output_mov = self.out_path(type='mov')
+        self.output_bin = self.out_path(type='bin')
         self.open_file()
         
     
-    def out_path(self):
-        
+    def out_path(self,type='mov'):
         path ,fmt = os.path.splitext(self.input_path)
-        re_path = path + '.mov'
+        if type == 'mov':
+            re_path = path + '.mov'
+        
+        elif type == 'bin':
+            re_path = path + '.bin'
         
         return re_path
         
@@ -41,7 +45,8 @@ class FacialPBExport():
             return
         
         self.set_maya_cam(0)
-        self.set_playblast(self.output_path)
+        self.set_playblast(self.output_mov)
+        self.expAnim()
         self.set_maya_cam(1)
         
     def set_maya_cam(self,value):
@@ -101,7 +106,70 @@ class FacialPBExport():
                 print 'playblast ERROR:',e
         
         return
-
+    
+    def getTransform(self,root):
+            obj = pm.ls(root,dag=True ,type='transform')
+            result = self.getKeyframes(obj)
+            
+            return result
+    
+    def getKeyframes(self,objs):
+            
+            all_key_frames = {}
+            attr_list =[]
+            time_list = []
+            value_list =[]
+            
+            for obj in objs:
+                anim_attr = pm.listAnimatable(obj)
+                for attr in anim_attr:
+                    num_key_frames = pm.keyframe(attr,q=True,keyframeCount=True)
+                    if (num_key_frames > 0):
+                        times = pm.keyframe(attr,q=True,index = (0,num_key_frames),timeChange=True)
+                        values = pm.keyframe(attr,q=True,index = (0,num_key_frames),valueChange=True)
+                        for i in range(0,num_key_frames):
+                            
+                            attr_list.append('%s'%attr)
+                            time_list.append(times[i])
+                            value_list.append(values[i])
+            
+            all_key_frames['Attribute'] = attr_list
+            all_key_frames['Time'] = time_list
+            all_key_frames['Value'] = value_list
+            
+            return all_key_frames
+    
+    
+    def writeFile(self,path,keyData):
+            
+        with open(path,'wb') as f:
+            f.write(cPickle.dumps(keyData,True))
+    
+        return
+    
+    def expAnim(self):
+        print 'export key frames'
+        
+        if 0 != len(pm.ls('ControllersParent')):
+            node = pm.ls('ControllersParent','EyeCntr_p')
+        
+        elif 0 != len(pm.ls('*:ControllersParent')):
+            node = pm.ls('*:ControllersParent','*:EyeCntr_p')
+            
+        else:
+            _logger.error('No object matches name: SnappersFacialRig')
+            return
+            
+        get_key_data = self.getTransform(node)
+        
+        
+        try:
+            self.writeFile(self.output_bin , get_key_data)
+        
+       
+        except Exception as e:
+            print 'ERROR'
+           
 '''
 path_list = ['D:/test/0035_0300_pc_facial_a_0006_v001.ma',
          'D:/test/0035_4000_npc_facial_f_0004_v001.ma',
